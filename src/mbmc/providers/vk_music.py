61760a34b96c977458e3fc1838d91b8ed4390464
Fabian Wunsch <fabian@uriah.heep.sax.de>
2025-11-22 16:25:45 +0100

    Solve VK bot challenges

--- src/mbmc/providers/vk_music.py
+++ src/mbmc/providers/vk_music.py
@@ -1,11 +1,14 @@
+import hashlib
 import json
 import re
+from functools import cache
 from typing import TypedDict, List
+from urllib.parse import urlparse, parse_qs, urlunparse, urlencode
 
 import requests
 from bs4 import BeautifulSoup, Tag
+from requests.cookies import RequestsCookieJar
 
-from mbmc.music_brainz import normalize_url
 from mbmc.providers._mb_link_types import (
     ARTIST_FREE_STREAMING,
     RELEASE_FREE_STREAMING,
@@ -25,12 +28,58 @@ class Playlist(TypedDict):
     coverUrl: str
 
 
+@cache
+def get_cookies() -> RequestsCookieJar:
+    request = requests.get(
+        "https://vk.com/challenge.html",
+        allow_redirects=True,
+    )
+    request.raise_for_status()
+    codes: list[str] = re.search(r"var codes = \[(.*?)];var", request.text).group(1)[1:-1].split("],[")
+    salt: str = ''
+    for code in codes:
+        init: int = int(re.search(r"return ([0-9-]+);", code).group(1))
+        for mutation in reversed(re.findall(r"\(function\(e\) ?\{(.*?)}\),", code)):
+            if "return e" in mutation:
+                parts = re.search(r"return e ?([-+^]) ?([0-9-]+);", mutation)
+                if parts is None:
+                    raise RuntimeError(f"Couldn't parse {mutation}")
+                operator = parts.group(1)
+                operand = int(parts.group(2))
+                if operator == "+":
+                    init += operand
+                elif operator == "-":
+                    init -= operand
+                elif operator == "^":
+                    init ^= operand
+            else:
+                for possibility in re.finditer(r'"([0-9-]+)":([0-9-]+)', mutation):
+                    if possibility.group(1) == str(init):
+                        init = int(possibility.group(2))
+                        break
+                else:
+                    raise ValueError("Could not find matching mutation")
+        salt += chr(init)
+    url = urlparse(request.url)
+    query = parse_qs(url.query)
+    hash429 = query.get("hash429")[0]
+    key = hashlib.md5(f"{hash429}:{salt}".encode()).hexdigest()
+    query["key"] = [key]
+    new_url = urlunparse(
+        (url.scheme, url.netloc, url.path, url.params, urlencode(query, doseq=True), url.fragment)
+    )
+    result = requests.get(new_url, allow_redirects=True)
+    result.raise_for_status()
+    return result.cookies
+
+
 def get_data(artist_name: str, type_: str) -> list[Playlist]:
     request = requests.get(
         f"https://vk.com/artist/{artist_name}/{type_}",
         headers={
             "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:143.0) Gecko/20100101 Firefox/143.0"
         },
+        cookies=get_cookies(),
     )
     request.raise_for_status()
     all_html = request.text
@@ -42,6 +91,7 @@ def get_data(artist_name: str, type_: str) -> list[Playlist]:
             "Content-Type": "application/x-www-form-urlencoded",
         },
         data={"al": "1", "section_id": section_id},
+        cookies=get_cookies(),
     )
     request.raise_for_status()
     text = request.text.replace("<!--", "", 1)
